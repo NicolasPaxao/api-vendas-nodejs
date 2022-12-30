8f30c90f900f2497192ec67573724819b949e3e3
DeehModesto <nicolaspaxao@gmail.com>
2022-12-30 16:06:42 -0300

    Criando as rotas de produtos

---
+++ src/modules/products/controllers/products_controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import CreateProductService from '../services/create_product_service';
+import DeleteProductService from '../services/delete_product_service';
+import ListProductsService from '../services/list_products_service';
+import ShowProductService from '../services/show_product_service';
+import UpdateProductService from '../services/update_product_service';
+
+export default class ProductsController {
+  public async index(response: Response): Promise<Response> {
+    const listProducts = new ListProductsService();
+    const products = await listProducts.execute();
+    return response.json(products);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showProduct = new ShowProductService();
+    const product = await showProduct.execute({ id });
+    return response.json(product);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+    const createProduct = new CreateProductService();
+    const product = await createProduct.execute({ name, price, quantity });
+    return response.json(product);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+    const { id } = request.params;
+    const updateProduct = new UpdateProductService();
+    const product = await updateProduct.execute({ id, name, price, quantity });
+    return response.json(product);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const deleteProduct = new DeleteProductService();
+    await deleteProduct.execute({ id });
+    return response.json({});
+  }
+}
