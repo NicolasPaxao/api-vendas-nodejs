061f9b999dca92ceecaf40ff82efe1990b18ad5d
DeehModesto <nicolaspaxao@gmail.com>
2023-01-04 17:10:25 -0300

    Criando a rota de upload

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { UsersController } from '../controllers/users_controller';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 import isAuthenticated from '../../../shared/http/middlewares/is_authenticated';
+import UserAvatarController from '../controllers/user_avatar_controller';
 
 export const usersRouter = Router();
 const userController = new UsersController();
+const avatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, userController.index);
 
@@ -20,4 +26,11 @@ usersRouter.post(
     ],
   }),
   userController.create,
+);
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  avatarController.update,
 );
